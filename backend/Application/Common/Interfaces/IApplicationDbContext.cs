d2fecf94f1088317d90b087c429a4202a1b0dba2
Jeppe Trip Kofoed <jtk@it-minds.dk>
2021-01-07 15:39:18 +0100

    started work on the add refill command.

--- backend/Application/Common/Interfaces/IApplicationDbContext.cs
+++ backend/Application/Common/Interfaces/IApplicationDbContext.cs
@@ -7,10 +7,12 @@ namespace Application.Common.Interfaces
 {
   public interface IApplicationDbContext
   {
-
-
     DbSet<ExampleEntity> ExampleEntities { get; set; }
     DbSet<ExampleEntityList> ExampleEntityLists { get; set; }
+    DbSet<Building> Buildings {get; set;}
+    DbSet<Coupon> Coupons { get; set; }
+    DbSet<Domain.Entities.Refill> Refills { get; set; }
+    DbSet<Truck> Trucks { get; set; }
     Task<int> SaveChangesAsync(CancellationToken cancellationToken);
   }
 }
---
+++ backend/Application/Refill/Commands/CreateRefill/CreateRefillCommand.cs
@@ -0,0 +1,53 @@
+using Application.Common.Interfaces;
+using Domain.Entities;
+using Domain.Enums;
+using MediatR;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Refill.Commands.CreateRefill
+{
+  public class CreateRefillCommand : IRequest<int>
+  {
+    public TankType TankType { get; set; }
+    public int TankNumber { get; set; }
+    public int Amount { get; set; }
+    public int CouponNumber { get; set; }
+    public DateTime Date { get; set; }
+    public FuelType FuelType { get; set; }
+    public TankState TankState { get; set; }
+
+    public class CreateRefillCommandHandler : IRequestHandler<CreateRefillCommand, int>
+    {
+      private readonly IApplicationDbContext _context;
+
+      public CreateRefillCommandHandler(IApplicationDbContext context)
+      {
+        _context = context;
+      }
+
+      public async Task<int> Handle(CreateRefillCommand request, CancellationToken cancellationToken)
+      {
+
+        var Building = _context.Buildings.First( x => x.Type == request.TankType && x.TankNumber == request.TankNumber);
+        var refill = new Domain.Entities.Refill
+        {
+          Amount = request.Amount,
+          CouponNumber = request.CouponNumber,
+          Date = request.Date,
+          Type = request.FuelType,
+          TankState = request.TankState,
+          Building = Building
+        };
+
+        _context.Refills.Add(refill);
+
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return refill.Id;
+      }
+    }
+  }
+}
