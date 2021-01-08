2f709fafab081c055512f79f96b650d82e477fe9
Jeppe Trip Kofoed <jtk@it-minds.dk>
2021-01-08 14:40:12 +0100

    added a few more rules to the command validator.

--- backend/Application/Refill/Commands/CreateRefill/CreateRefillCommandValidator.cs
+++ backend/Application/Refill/Commands/CreateRefill/CreateRefillCommandValidator.cs
@@ -6,17 +6,26 @@ namespace Application.Refill.Commands.CreateRefill
   {
     public CreateRefillCommandValidator()
     {
+      RuleFor(e => e.TruckId)
+        .NotNull();
+      RuleFor(e => e.TankNumber)
+        .NotNull();
+      RuleFor(e => e.Amount)
+        .NotNull()
+        .GreaterThan(0);
+      RuleFor(e => e.CouponNumber)
+        .NotNull();
       RuleFor(e => e.TankType)
-          .IsInEnum()
-          .NotNull();
+        .IsInEnum()
+        .NotNull();
       RuleFor(e => e.FuelType)
-          .IsInEnum()
-          .NotNull();
+        .IsInEnum()
+        .NotNull();
       RuleFor(e => e.TankState)
-          .IsInEnum()
-          .NotNull();
+        .IsInEnum()
+        .NotNull();
       RuleFor(e => e.Date)
-          .NotNull();
+        .NotNull();
     }
   }
 }
