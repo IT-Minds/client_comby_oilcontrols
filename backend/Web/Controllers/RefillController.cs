ae9ed332db5367d79df1502df8b8783949103bf7
Jeppe Trip Kofoed <jtk@it-minds.dk>
2021-01-11 14:16:32 +0100

    gathered refill commands and queries under the same parent directory and fixed breaking changes.

--- backend/Web/Controllers/RefillController.cs
+++ backend/Web/Controllers/RefillController.cs
@@ -1,4 +1,3 @@
-using Application.Refill.Commands.CreateRefill;
 using Application.Common.Interfaces.Pagination;
 using Application.Refills.Commands.CreateRefill;
 using Application.Refills.Queries.GetRefills;
@@ -26,7 +25,7 @@ namespace Web.Controllers
         TankNumber = tankNumber
       });
     }
-    
+
     [HttpGet]
     public async Task<ActionResult<PageResult<RefillDto>>> Get(
       [FromQuery] int needle, [FromQuery] int size, [FromQuery] int? skip = 0
