039542a334c952df849a2e5e1a7adfaba8142a4e
Jeppe Trip Kofoed <jtk@it-minds.dk>
2021-01-11 12:56:40 +0100

    added a controller for the query.

--- backend/Web/Controllers/RefillController.cs
+++ backend/Web/Controllers/RefillController.cs
@@ -1,4 +1,6 @@
-using Application.Refill.Commands.CreateRefill;
+using Application.Common.Interfaces.Pagination;
+using Application.Refills.Queries.GetRefills;
+using Application.Refills.Queries.GetRefills.Location;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
@@ -6,10 +8,15 @@ namespace Web.Controllers
 {
   public class RefillController : ApiControllerBase
   {
-    [HttpPost]
-    public async Task<ActionResult<int>> Create(CreateRefillCommand command)
-    {
-      return await Mediator.Send(command);
+    [HttpGet]
+    public async Task<ActionResult<PageResult<RefillDto>>> Get(
+      [FromQuery] string needle, [FromQuery] int size, [FromQuery] int? skip = 0
+    ){
+      return await Mediator.Send(new GetRefillsLocationQuery{
+        Size = size,
+        Needle = needle,
+        Skip = skip
+      });
     }
   }
 }
