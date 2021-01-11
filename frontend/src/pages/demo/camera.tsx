ed3ca66e52d7540cfa3779844c61fffcc0646984
Daniel Velando <dve@it-minds.dk>
2021-01-11 08:32:42 +0100

    Set camera component

--- frontend/src/pages/demo/camera.tsx
+++ frontend/src/pages/demo/camera.tsx
@@ -1,11 +1,12 @@
 import { Box, Container } from "@chakra-ui/react";
+import CameraComp from "components/Camera/CameraComponent";
 import { NextPage } from "next";
 
 const DemoPage: NextPage = () => {
   return (
     <Container maxW="xl" centerContent>
       <Box padding="4" bg="gray.100" maxW="6xl" maxH="4xl" resize="both" overflow="auto">
-        {/* <!-- CAMERA COMPONENT HERE--> */}
+        <CameraComp buildTime={0} />
       </Box>
     </Container>
   );
