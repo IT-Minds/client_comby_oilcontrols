7b09e1ebd77bbcf20a06a7363b13aa6176140cf2
Jeppe Trip Kofoed <jtk@it-minds.dk>
2021-01-11 14:39:02 +0100

    made it such that the refill query takes the tank type and number as query parameters and the needle is the date created.

--- backend/Web/Controllers/RefillController.cs
+++ backend/Web/Controllers/RefillController.cs
@@ -2,7 +2,9 @@ using Application.Common.Interfaces.Pagination;
 using Application.Refills.Commands.CreateRefill;
 using Application.Refills.Queries.GetRefills;
 using Application.Refills.Queries.GetRefills.Location;
+using Domain.Enums;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Threading.Tasks;
 
 namespace Web.Controllers
@@ -17,11 +19,13 @@ namespace Web.Controllers
 
     [HttpGet]
     public async Task<ActionResult<PageResult<RefillDto>>> Get(
-      [FromQuery] int needle, [FromQuery] int size, [FromQuery] int? skip = 0
-    ){
-      return await Mediator.Send(new GetRefillsLocationQuery{
+      [FromQuery] TankType tankType, [FromQuery] int tankNumber, [FromQuery] string needle, [FromQuery] int size, [FromQuery] int? skip = 0
+    )
+    {
+      return await Mediator.Send(new GetRefillsLocationQuery
+      {
         Size = size,
-        Needle = needle,
+        Needle = new System.DateTimeOffset(Int64.Parse(needle), new TimeSpan()),
         Skip = skip
       });
     }
