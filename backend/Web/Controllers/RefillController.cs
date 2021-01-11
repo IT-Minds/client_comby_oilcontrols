3b3c5ff32e6ce76498f2d3a7c137c2bcfe829bb0
Jeppe Trip Kofoed <jtk@it-minds.dk>
2021-01-11 12:56:40 +0100

    added a controller for the query.

--- backend/Web/Controllers/RefillController.cs
+++ backend/Web/Controllers/RefillController.cs
@@ -11,12 +11,6 @@ namespace Web.Controllers
 {
   public class RefillController : ApiControllerBase
   {
-    [HttpPost]
-    public async Task<ActionResult<int>> Create(CreateRefillCommand command)
-    {
-      return await Mediator.Send(command);
-    }
-
     [HttpGet]
     public async Task<ActionResult<PageResult<RefillDto>>> Get(
       [FromQuery] TankType tankType, [FromQuery] int tankNumber, [FromQuery] string needle, [FromQuery] int size, [FromQuery] int? skip = 0
