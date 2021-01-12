9e8a62c1d9909c9eafebaa294f63848359756a12
Daniel Velando <dve@it-minds.dk>
2021-01-12 15:30:41 +0100

    Pass in test data for reportForm

--- frontend/src/pages/demo/reporting.tsx
+++ frontend/src/pages/demo/reporting.tsx
@@ -1,15 +1,32 @@
-import { Container, VStack } from "@chakra-ui/react";
+import { Container } from "@chakra-ui/react";
+import { ReportForm } from "components/Reporting/ReportForm";
 import ReportingComp from "components/Reporting/ReportingComponent";
 import { NextPage } from "next";
+import { useState } from "react";
 
 const DemoPage: NextPage = () => {
+  const [reportForm, setReportForm] = useState<ReportForm>(null);
+
   return (
     <Container maxW="xl" centerContent>
       <ReportingComp
+        submitCallback={setReportForm}
         carId="12345"
-        locations={["Building 1", "Building 2", "Building 3"]}
-        couponNumbers={["Coupon 1", "Coupon 2", "Coupon 3"]}
-        fillType={["Oil", "Gas", "Whatever"]}></ReportingComp>
+        locations={[
+          { name: "Build 1", id: "1" },
+          { name: "Build 2", id: "2" },
+          { name: "Build 3", id: "3" }
+        ]}
+        couponNumbers={[
+          { name: "Coupon 1", id: "1" },
+          { name: "Coupon 2", id: "2" },
+          { name: "Coupon 3", id: "3" }
+        ]}
+        fillTypes={[
+          { name: "Oil", id: "1" },
+          { name: "Gas", id: "2" },
+          { name: "Something else", id: "3" }
+        ]}></ReportingComp>
     </Container>
   );
 };
