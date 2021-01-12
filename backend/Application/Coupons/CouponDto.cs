7bf57a5794b6ab1db62b4a6719891f6437c40133
Jeppe Trip Kofoed <jtk@it-minds.dk>
2021-01-12 13:57:06 +0100

    removed the unnecessary mappings from the dto.

--- backend/Application/Coupons/CouponDto.cs
+++ backend/Application/Coupons/CouponDto.cs
@@ -11,16 +11,5 @@ namespace Application.Coupons.Queries.GetCoupons
     public int CouponNumber { get; set; }
     public int TruckId { get; set; }
     public CouponStatus Status { get; set; }
-
-    public void Mapping(Profile profile)
-    {
-      profile.CreateMap<Coupon, CouponDto>()
-        .ForMember(dest => dest.CouponNumber,
-          map => map.MapFrom(from => from.CouponNumber))
-        .ForMember(dest => dest.TruckId,
-          map => map.MapFrom(from => from.TruckId))
-        .ForMember(dest => dest.Status,
-          map => map.MapFrom(from => from.Status));
-    }
   }
 }
