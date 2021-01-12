de9d9dbb95d118e8f76cc7bb537fdfb83cb4904a
Daniel Velando <dve@it-minds.dk>
2021-01-12 11:16:27 +0100

    Set carId as prop

--- frontend/src/components/Reporting/ReportingComponent.tsx
+++ frontend/src/components/Reporting/ReportingComponent.tsx
@@ -14,18 +14,20 @@ import { FC } from "react";
 import styles from "./styles.module.css";
 
 type Props = {
+  carId: string;
   locations: string[];
   couponNumbers: string[];
   fillType: string[];
 };
 
 const ReportingComp: FC<Props> = ({ locations, couponNumbers, fillType }) => {
+const ReportingComp: FC<Props> = ({ carId, locations, couponNumbers, fillType }) => {
   return (
     <Stack>
       <form>
         <FormControl id="car-id">
           <FormLabel>Car id.</FormLabel>
-          <Input type="text" isReadOnly isDisabled value="123" />
+          <Input type="text" isReadOnly isDisabled value={carId} />
         </FormControl>
 
         <FormControl id="location">
--- frontend/src/pages/demo/reporting.tsx
+++ frontend/src/pages/demo/reporting.tsx
@@ -6,6 +6,7 @@ const DemoPage: NextPage = () => {
   return (
     <Container maxW="xl" centerContent>
       <ReportingComp
+        carId="12345"
         locations={["Building 1", "Building 2", "Building 3"]}
         couponNumbers={["Coupon 1", "Coupon 2", "Coupon 3"]}
         fillType={["Oil", "Gas", "Whatever"]}></ReportingComp>
