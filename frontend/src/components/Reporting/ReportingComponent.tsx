d718c4f6ec99ed2820f5a58df859528682b57b37
Daniel Velando <dve@it-minds.dk>
2021-01-12 11:07:10 +0100

    Setup form control
    With the input fields required

---
+++ frontend/src/components/Reporting/ReportingComponent.tsx
@@ -0,0 +1,82 @@
+import {
+  Button,
+  Checkbox,
+  FormControl,
+  FormErrorMessage,
+  FormHelperText,
+  FormLabel,
+  Input,
+  Select,
+  Stack
+} from "@chakra-ui/react";
+import { FC } from "react";
+
+import styles from "./styles.module.css";
+
+type Props = {
+  locations: string[];
+  couponNumbers: string[];
+  fillType: string[];
+};
+
+const ReportingComp: FC<Props> = ({ locations, couponNumbers, fillType }) => {
+  return (
+    <Stack>
+      <form>
+        <FormControl id="car-id">
+          <FormLabel>Car id.</FormLabel>
+          <Input type="text" isReadOnly isDisabled value="123" />
+        </FormControl>
+
+        <FormControl id="location">
+          <FormLabel>Select location:</FormLabel>
+          <Select placeholder="Select location">
+            {locations.map(path => (
+              <option key={path} value={path}>
+                {path}
+              </option>
+            ))}
+          </Select>
+        </FormControl>
+
+        <FormControl id="coupon-number">
+          <FormLabel>Select coupon number:</FormLabel>
+          <Select>
+            {couponNumbers.map(path => (
+              <option key={path} value={path}>
+                {path}
+              </option>
+            ))}
+          </Select>
+        </FormControl>
+
+        <FormControl id="fill-number">
+          <FormLabel>Select fill type:</FormLabel>
+          <Select>
+            {fillType.map(path => (
+              <option key={path} value={path}>
+                {path}
+              </option>
+            ))}
+          </Select>
+        </FormControl>
+
+        <FormControl id="first-name" isRequired>
+          <FormLabel>Amount filled:</FormLabel>
+          <Input placeholder="Enter amount in liters" />
+        </FormControl>
+
+        <FormControl id="special-fill">
+          <FormLabel>Is special fill:</FormLabel>
+          <Checkbox></Checkbox>
+        </FormControl>
+
+        <Button mt={4} colorScheme="teal" type="submit">
+          Submit
+        </Button>
+      </form>
+    </Stack>
+  );
+};
+
+export default ReportingComp;
