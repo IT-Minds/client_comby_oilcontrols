1be0c522140f1ead118eb9ea54ec082196c2dc83
Daniel Velando <dve@it-minds.dk>
2021-01-12 11:07:26 +0100

    Setup route and show component

---
+++ frontend/src/pages/demo/reporting.tsx
@@ -0,0 +1,16 @@
+import { Container, VStack } from "@chakra-ui/react";
+import ReportingComp from "components/Reporting/ReportingComponent";
+import { NextPage } from "next";
+
+const DemoPage: NextPage = () => {
+  return (
+    <Container maxW="xl" centerContent>
+      <ReportingComp
+        locations={["Building 1", "Building 2", "Building 3"]}
+        couponNumbers={["Coupon 1", "Coupon 2", "Coupon 3"]}
+        fillType={["Oil", "Gas", "Whatever"]}></ReportingComp>
+    </Container>
+  );
+};
+
+export default DemoPage;
