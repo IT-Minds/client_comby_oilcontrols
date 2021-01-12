b192414ebc48e77d97c575f3e38b47d2b659bc79
Martin Johannesson <mjh@it-minds.dk>
2021-01-12 16:11:16 +0100

    Utilising Ooptions from configuration to get path

--- backend/Application/Coupon/Commands/SaveCouponImage/SaveCouponImageCommand.cs
+++ backend/Application/Coupon/Commands/SaveCouponImage/SaveCouponImageCommand.cs
@@ -1,13 +1,13 @@
 using Application.Common.Interfaces;
 using MediatR;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
 using System;
 using System.IO;
-using System.Linq;
+using Application.Common.Options;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
-
 namespace Application.Coupons.Commands.SaveCouponImage
 {
   public class SaveCouponImageCommand : IRequest<string>
@@ -15,24 +15,28 @@ namespace Application.Coupons.Commands.SaveCouponImage
     public int RefillId { get; set; }
     public IFormFile File { get; set; }
 
-
     public class SaveCouponImageCommandHandler : IRequestHandler<SaveCouponImageCommand, string>
     {
       private readonly IApplicationDbContext _context;
-      public SaveCouponImageCommandHandler(IApplicationDbContext context)
+      private readonly FileDriveOptions _options;
+
+      public SaveCouponImageCommandHandler(IApplicationDbContext context, IOptions<FileDriveOptions> options)
       {
         _context = context;
+        _options = options.Value;
       }
+
 
       public async Task<string> Handle(SaveCouponImageCommand request, CancellationToken cancellationToken)
       {
-        var refill = _context.Refills.Where(x => x.Id == request.RefillId);
-        if(refill.Count() == 0){
+        var refill = await _context.Refills.FindAsync(request.RefillId);
+        if(refill == null){
           throw new ArgumentException("No refill with ID: "+request.RefillId);
         }
+
         String imgType;
-        Regex png = new Regex(@"image/png");
-        Regex webp = new Regex(@"image/webp");
+        Regex png = new Regex(@"^image\/png$");
+        Regex webp = new Regex(@"^image\/webp$");
 
         if(png.IsMatch(request.File.ContentType)){
           imgType = "png";
@@ -43,14 +47,13 @@ namespace Application.Coupons.Commands.SaveCouponImage
           throw new ArgumentException("Invalid content type.");
         }
 
-        var folderpath = System.Environment.GetEnvironmentVariable("COUPON_PATH");
-        var filename = request.RefillId+"."+imgType;       
-        string filePath = Path.Combine(folderpath, filename);
-        
+        var filename = request.RefillId+"."+imgType;
+        string filePath = Path.Combine(_options.Path, filename);
+
         if(System.IO.File.Exists(filePath)){
           throw new ArgumentException("Image with "+request.RefillId+" already exists.");
         }
-        
+
         using (Stream fileStream = new FileStream(filePath, FileMode.Create))
         {
           await request.File.CopyToAsync(fileStream);
