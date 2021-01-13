fd7b212963a95fd966a6a9b0f5d2dad28b62f9d1
Jeppe Trip Kofoed <jtk@it-minds.dk>
2021-01-13 09:08:14 +0100

    problems with the migrations at this point.

--- backend/Infrastructure/Persistence/ApplicationDbContext.cs
+++ backend/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -30,6 +30,9 @@ namespace Infrastructure.Persistence
     public DbSet<Refill> Refills { get; set; }
     public DbSet<Truck> Trucks { get; set; }
     public DbSet<Route> Routes { get; set; }
+    public DbSet<TruckState> DailyTruckStates { get; set; }
+    public DbSet<TruckRefill> TruckRefills { get; set; }
+
     public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
     {
       foreach (var entry in ChangeTracker.Entries<AuditableEntity>())
