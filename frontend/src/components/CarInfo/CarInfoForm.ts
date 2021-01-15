6d562a99e403968fa7ee9c723e4123b5322f9333
Daniel Velando <dve@it-minds.dk>
2021-01-15 14:20:12 +0100

    Form interface

---
+++ frontend/src/components/CarInfo/CarInfoForm.ts
@@ -0,0 +1,10 @@
+export interface CarInfoForm {
+  carId: string;
+  morning: string;
+  evening: string;
+  amountFilled: string;
+  date: string;
+  cardNumber: string;
+  fuelType: number;
+  headCount: string;
+}
