02c663a035fa75dfae91f86a6c822d298a860be0
Jeppe Trip Kofoed <jtk@it-minds.dk>
2021-01-15 13:19:11 +0100

    added the demo page.

---
+++ frontend/src/pages/demo/AddLocaleMetaData.tsx
@@ -0,0 +1,12 @@
+import { Container } from "@chakra-ui/react";
+import { NextPage } from "next";
+
+const DemoPage: NextPage = () => {
+  return (
+    <Container maxW="xl" centerContent>
+      TEST Data.
+    </Container>
+  );
+};
+
+export default DemoPage;
