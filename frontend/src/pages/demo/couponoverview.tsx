210c2941fa5004dc4f1ae5c90605c5ea327cdaed
Daniel Velando <54571314+DanielVelando@users.noreply.github.com>
2021-01-15 09:42:01 +0100

    Feature/co 60 (#44)
    Co-authored-by: Martin Johannesson <mjh@it-minds.dk>

---
+++ frontend/src/pages/demo/couponoverview.tsx
@@ -0,0 +1,21 @@
+import { Box, Container } from "@chakra-ui/react";
+import CouponOverviewComp from "components/CouponOverview/CouponOverviewComponent";
+import { NextPage } from "next";
+
+const DemoPage: NextPage = () => {
+  return (
+    <Container maxW="xl" centerContent>
+      <Box padding="4" bg="gray.100" maxW="6xl" maxH="4xl" resize="both" overflow="auto">
+        <CouponOverviewComp
+          car="123"
+          coupons={[
+            { start: 1, end: 7, id: "1" },
+            { start: 13, end: 25, id: "2" },
+            { start: 2173, end: 2340, id: "3" }
+          ]}></CouponOverviewComp>
+      </Box>
+    </Container>
+  );
+};
+
+export default DemoPage;
