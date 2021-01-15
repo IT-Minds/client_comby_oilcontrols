68bfab764016d4d3c27c4173150b79be204cd547
Jeppe Trip Kofoed <jtk@it-minds.dk>
2021-01-15 14:09:55 +0100

    setup a basic form structure.

--- frontend/src/pages/demo/AddLocaleMetaData.tsx
+++ frontend/src/pages/demo/AddLocaleMetaData.tsx
@@ -1,10 +1,14 @@
-import { Container } from "@chakra-ui/react";
+import { Box, Container } from "@chakra-ui/react";
+import LocaleMetaDataForm from "components/LocaleMetaDataForm/LocaleMetaDataForm";
 import { NextPage } from "next";
+import React from "react";
 
 const DemoPage: NextPage = () => {
   return (
     <Container maxW="xl" centerContent>
-      TEST Data.
+      <Box padding="4" bg="gray.100" maxW="6xl" maxH="4xl" resize="both" overflow="auto">
+        <LocaleMetaDataForm id={"someId"}></LocaleMetaDataForm>
+      </Box>
     </Container>
   );
 };
