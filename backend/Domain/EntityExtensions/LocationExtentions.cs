9ba649fb0dc82f0db930004d987b1cbe17d080d9
Jeppe Trip Kofoed <jtk@it-minds.dk>
2021-01-18 14:34:59 +0100

    added in a draft for the extension to calculate the date when the minimum fuel levels have been reached.

--- backend/Domain/EntityExtensions/LocationExtentions.cs
+++ backend/Domain/EntityExtensions/LocationExtentions.cs
@@ -33,5 +33,27 @@ namespace Domain.EntityExtensions
 
       return fuelConsumed / heatingDegree;
     }
+
+    public static DateTime PredictDayReachingMinimumFuelLevel(this Location location)
+    {
+      double limit = location.FuelTank.MinimumFuelAmount;
+      var sortedRefills = location.Refills.OrderBy(x => x.ActualDeliveryDate);
+      var newestRefill = sortedRefills.Last();
+      var refillDate = newestRefill.ActualDeliveryDate;
+      var fuelAmount = newestRefill.EndAmount;
+      var fuelConsumption = location.FuelConsumptionPerDegreeOfHeating();
+
+      var currentDate = refillDate;
+      do
+      {
+        currentDate = currentDate.AddDays(1);
+        var heatdegree = location.Region.DailyTemperatureEstimate(currentDate);
+        var fuelConsumed = heatdegree * fuelConsumption;
+        fuelAmount = fuelAmount - fuelConsumed;
+      }
+      while (fuelAmount > location.FuelTank.MinimumFuelAmount);
+
+      return currentDate.AddDays(-1);
+    }
   }
 }
