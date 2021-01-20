de5092af0b11b9a017340e14b3ca6a7f57f99888
Daniel Velando <dve@it-minds.dk>
2021-01-20 10:31:09 +0100

    Use date-picker

--- frontend/src/components/OrderRefill/OrderRefillComp.tsx
+++ frontend/src/components/OrderRefill/OrderRefillComp.tsx
@@ -1,3 +1,5 @@
+import "react-datepicker/dist/react-datepicker.css";
+
 import {
   AlertDialog,
   AlertDialogBody,
@@ -15,25 +17,33 @@ import {
   TableCaption,
   Tbody,
   Td,
+  Text,
   Th,
   Thead,
   Tr,
-  useDisclosure,
-  VStack
+  useDisclosure
 } from "@chakra-ui/react";
 import { Locale } from "i18n/Locale";
 import { useI18n } from "next-rosetta";
+import { stringify } from "querystring";
 import React, { FC, FormEvent, useCallback, useRef, useState } from "react";
+import ReactDatePicker from "react-datepicker";
 import { MdCheck } from "react-icons/md";
 import DropdownType from "types/DropdownType";
 import { logger } from "utils/logger";
 
+import DatePicker from "./date-picker";
 import { OrderRefillForm } from "./OrderRefillForm";
 
 type Props = {
   submitCallback: (orderRefillForm: OrderRefillForm) => void;
   locations: DropdownType[];
 };
+
+interface LocationDates {
+  locationId: string;
+  date: Date;
+}
 
 const OrderRefillComp: FC<Props> = ({ submitCallback, locations = [] }) => {
   const [localOrderRefillForm, setLocalOrderRefillForm] = useState<OrderRefillForm>({
@@ -46,6 +56,9 @@ const OrderRefillComp: FC<Props> = ({ submitCallback, locations = [] }) => {
 
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [isLoading, setLoading] = useState(false);
+  const [selectedlocationId, setSelectedLocationId] = useState(null);
+  const [modalDate, setModalDate] = useState(null);
+  const [dates, setDates] = useState<LocationDates[]>([]);
 
   const { t } = useI18n<Locale>();
 
@@ -57,6 +70,21 @@ const OrderRefillComp: FC<Props> = ({ submitCallback, locations = [] }) => {
       return form;
     });
   }, []);
+
+  const addDateToLocation = useCallback(() => {
+    const exists = dates.find(d => d.locationId === selectedlocationId);
+    if (exists) {
+      exists.date = modalDate;
+      const index = dates.findIndex(d => d.locationId === selectedlocationId);
+      const clone = [...dates];
+      clone[index] = exists;
+      setDates(clone);
+    } else {
+      setDates(oldArray => [...oldArray, { locationId: selectedlocationId, date: modalDate }]);
+    }
+
+    onClose();
+  }, [dates, selectedlocationId, modalDate]);
 
   const handleSubmit = useCallback(
     (event: FormEvent<HTMLFormElement>) => {
@@ -70,81 +98,80 @@ const OrderRefillComp: FC<Props> = ({ submitCallback, locations = [] }) => {
 
   return (
     <Container>
-      <Table variant="striped" colorScheme="teal">
-        <TableCaption placement="top">Filling overview</TableCaption>
-        <Thead>
-          <Tr>
-            <Th>Location</Th>
-            <Th>Date</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {locations.map(location => {
-            return (
-              <Tr key={location.id}>
-                <Td>{location.name}</Td>
-                <Td>
-                  <Button colorScheme="blue" onClick={onOpen} isLoading={isLoading}>
-                    Choose date
-                  </Button>
-                </Td>
-              </Tr>
-            );
-          })}
-        </Tbody>
-      </Table>
+      <form>
+        <Table variant="striped" colorScheme="teal">
+          <TableCaption placement="top">Filling overview</TableCaption>
+          <Thead>
+            <Tr>
+              <Th>Location</Th>
+              <Th>Date</Th>
+              <Th>Selected Date</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {locations.map(location => {
+              return (
+                <Tr key={location.id}>
+                  <Td>{location.name}</Td>
+                  <Td>
+                    <Button
+                      colorScheme="blue"
+                      onClick={() => {
+                        setSelectedLocationId(location.id);
+                        onOpen();
+                      }}
+                      isLoading={isLoading}>
+                      {dates.find(d => d.locationId === location.id)?.date
+                        ? "Change date"
+                        : "Choose date"}
+                    </Button>
+                  </Td>
+                  <Td>
+                    {dates.find(d => d.locationId === location.id)?.date.toDateString() ?? ""}
+                  </Td>
+                </Tr>
+              );
+            })}
+          </Tbody>
+        </Table>
 
-      <AlertDialog isOpen={isOpen} leastDestructiveRef={cancelRef} onClose={onClose} isCentered>
-        <AlertDialogOverlay>
-          <AlertDialogContent>
-            <AlertDialogHeader fontSize="lg" fontWeight="bold">
-              {t("coupons.invalidate.invalidate")}
-            </AlertDialogHeader>
+        <AlertDialog isOpen={isOpen} leastDestructiveRef={cancelRef} onClose={onClose} isCentered>
+          <AlertDialogOverlay>
+            <AlertDialogContent>
+              <AlertDialogHeader fontSize="lg" fontWeight="bold">
+                {t("coupons.invalidate.invalidate")}
+              </AlertDialogHeader>
 
-            <AlertDialogBody>{t("coupons.invalidate.confirm", {})}</AlertDialogBody>
+              <AlertDialogBody>
+                <DatePicker
+                  selectedDate={
+                    dates?.find(d => d.locationId === selectedlocationId)?.date ?? modalDate
+                  }
+                  onChange={date => setModalDate(date)}
+                  showPopperArrow={false}
+                />
+              </AlertDialogBody>
 
-            <AlertDialogFooter>
-              <Button ref={cancelRef} onClick={onClose}>
-                {t("actions.cancel")}
-              </Button>
-              <Button colorScheme="red" onClick={null} ml={3}>
-                {t("actions.invalidate")}
-              </Button>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialogOverlay>
-      </AlertDialog>
+              <AlertDialogFooter>
+                <Button ref={cancelRef} onClick={onClose}>
+                  {t("actions.cancel")}
+                </Button>
+                <Button colorScheme="red" onClick={() => addDateToLocation()} ml={3}>
+                  {t("actions.invalidate")}
+                </Button>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialogOverlay>
+        </AlertDialog>
 
-      {/* <form onSubmit={handleSubmit}>
-        <VStack align="center" justify="center">
-          <FormControl
-            isInvalid={
-              formSubmitAttempts > 0 &&
-              locations.every(b => localOrderRefillForm.locationId !== b.id)
-            }
-            isRequired>
-            <FormLabel>Select location:</FormLabel>
-            <Select
-              placeholder="Select location"
-              onChange={e => updateLocalForm(e.target.value, "locationId")}>
-              {locations.map(location => (
-                <option key={location.id} value={location.id}>
-                  {location.name}
-                </option>
-              ))}
-            </Select>
-            <FormErrorMessage>Please select a location</FormErrorMessage>
-          </FormControl>
-
-          <Button
-            colorScheme="green"
-            type="submit"
-            rightIcon={<MdCheck />}
-            onClick={() => setFormSubmitAttempts(x => x + 1)}>
-            Submit
-          </Button>
-        </VStack>
-      </form> */}
+        <Button
+          colorScheme="green"
+          type="submit"
+          rightIcon={<MdCheck />}
+          onClick={() => setFormSubmitAttempts(x => x + 1)}>
+          Submit
+        </Button>
+      </form>
     </Container>
   );
 };
