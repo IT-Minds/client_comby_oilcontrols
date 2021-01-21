5379bdbb64f21cb2fb8e48cd9c9163aae6df5a05
Daniel Velando <dve@it-minds.dk>
2021-01-21 13:53:11 +0100

    setup page

---
+++ frontend/src/pages/demo/co100locationhistory.tsx
@@ -0,0 +1,32 @@
+import { Box, Container, useColorModeValue, useToast } from "@chakra-ui/react";
+import LocationHistoryComp from "components/LocationHistory/LocationHistoryComp";
+import { Locale } from "i18n/Locale";
+import { GetStaticProps, NextPage } from "next";
+import { I18nProps } from "next-rosetta";
+
+const DemoPage: NextPage = () => {
+  const toast = useToast();
+  const bg = useColorModeValue("gray.100", "gray.700");
+
+  return (
+    <Container maxW="xl" centerContent>
+      <Box padding="4" bg={bg} maxW="6xl" maxH="4xl" resize="both" overflow="auto">
+        <LocationHistoryComp
+          locationHistories={[
+            { name: "Location 1", locationId: 1 },
+            { name: "Location 2", locationId: 2 },
+            { name: "Location 3", locationId: 3 }
+          ]}></LocationHistoryComp>
+      </Box>
+    </Container>
+  );
+};
+
+export const getStaticProps: GetStaticProps<I18nProps<Locale>> = async context => {
+  const locale = context.locale || context.defaultLocale;
+
+  const { table = {} } = await import(`../../i18n/${locale}`);
+  return { props: { table } };
+};
+
+export default DemoPage;
