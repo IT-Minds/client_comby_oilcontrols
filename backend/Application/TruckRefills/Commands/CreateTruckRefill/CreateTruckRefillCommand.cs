8472961f421eaf7b834c41c4aa2db9449ca9c433
Jeppe Trip Kofoed <jtk@it-minds.dk>
2021-01-21 15:20:29 +0100

    setup command to create a new TruckRefill.

---
+++ backend/Application/TruckRefills/Commands/CreateTruckRefill/CreateTruckRefillCommand.cs
@@ -0,0 +1,58 @@
+using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Domain.Entities;
+using Domain.Enums;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.TruckRefills.Commands.CreateTruckRefill
+{
+  public class CreateTruckRefillCommand : IRequest<int>
+  {
+    public int TruckId { get; set; }
+    public DateTime TimeStamp { get; set; }
+    public int FuelCardNumber { get; set; }
+    public double Amount { get; set; }
+    public FuelType FuelType { get; set; }
+
+    public class CreateTruckRefillCommandHandler : IRequestHandler<CreateTruckRefillCommand, int>
+    {
+      private readonly IApplicationDbContext _context;
+
+      public CreateTruckRefillCommandHandler(IApplicationDbContext context)
+      {
+        _context = context;
+      }
+      public async Task<int> Handle(CreateTruckRefillCommand request, CancellationToken cancellationToken)
+      {
+        var truck = await _context.Trucks
+        .Include(x => x.DailyStates)
+        .FirstOrDefaultAsync(x => x.Id == request.TruckId);
+        if (truck == null)
+        {
+          throw new NotFoundException(nameof(truck), request.TruckId);
+        }
+
+        var dailyState = truck.DailyStates.FirstOrDefault(x => x.Date.DayOfYear == request.TimeStamp.DayOfYear && x.Date.Year == request.TimeStamp.Year);
+        if (truck == null)
+        {
+          throw new NotFoundException(nameof(dailyState), "For truck with id: " + request.TruckId + " on date: " + request.TimeStamp);
+        }
+        var truckRefill = new TruckRefill
+        {
+          TimeStamp = request.TimeStamp,
+          FuelCardNumber = request.FuelCardNumber,
+          Amount = request.Amount,
+          FuelType = request.FuelType,
+          TruckDailyStateId = dailyState.Id
+        };
+        await _context.SaveChangesAsync(cancellationToken);
+        return truckRefill.Id;
+      }
+    }
+  }
+}
