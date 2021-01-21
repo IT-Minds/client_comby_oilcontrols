8a4ad7bf9691760996d439408aef37d6b4ed3360
Martin Johannesson <mjh@it-minds.dk>
2021-01-21 21:32:45 +0100

    CO-29 UniConta integration added with base data model.

---
+++ backend/Application/Common/Interfaces/IUniContaService.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UniContaDomain.Entities;
+
+namespace Application.Common.Interfaces
+{
+  public interface IUniContaService
+  {
+    Task<bool> Login();
+    Task<List<Debtor>> GetDebtors();
+  }
+}
---
+++ backend/Infrastructure/Options/UniContaOptions.cs
@@ -0,0 +1,11 @@
+namespace Infrastructure.Options
+{
+  public class UniContaOptions
+  {
+    public const string UniConta = "UniConta";
+
+    public string ApiGuid { get; set; }
+    public string ApiUser { get; set; }
+    public string ApiPass { get; set; }
+  }
+}
