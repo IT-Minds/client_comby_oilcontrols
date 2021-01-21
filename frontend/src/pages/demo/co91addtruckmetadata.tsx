dcf95843e1fc6909855e9eca1063439cc4d945fd
Daniel Velando <dve@it-minds.dk>
2021-01-21 13:15:27 +0100

    Prepare backend integration

--- frontend/src/pages/demo/co91addtruckmetadata.tsx
+++ frontend/src/pages/demo/co91addtruckmetadata.tsx
@@ -1,32 +1,46 @@
 import { Box, Container, useColorModeValue, useToast } from "@chakra-ui/react";
 import AddTruckMetaData from "components/TruckMetaData/AddTruckMetaData";
-import { AddTruckMetaDataForm } from "components/TruckMetaData/AddTruckMetaDataForm";
+import { useOffline } from "hooks/useOffline";
 import { Locale } from "i18n/Locale";
 import { GetStaticProps, NextPage } from "next";
 import { I18nProps } from "next-rosetta";
-import { useEffect, useState } from "react";
+import { useCallback } from "react";
 
 const DemoPage: NextPage = () => {
-  const [addTruckMetaDataForm, setTruckMetaDataForm] = useState<AddTruckMetaDataForm>(null);
   const toast = useToast();
+  const { awaitCallback } = useOffline();
   const bg = useColorModeValue("gray.100", "gray.700");
 
-  useEffect(() => {
-    if (addTruckMetaDataForm) {
-      toast({
-        title: "Truck meta data added",
-        description: JSON.stringify(addTruckMetaDataForm),
-        status: "success",
-        duration: 9000,
-        isClosable: true
-      });
-    }
-  }, [addTruckMetaDataForm]);
+  const saveForm = useCallback(
+    async truckMetaDataForm => {
+      awaitCallback(async () => {
+        // TODO: WAITING FOR BACKEND COMMAND + INTERFACE
+        // const client = await genRefillClient();
+        // await client.create(
+        //   new CreateRefillCommand({
+        //     truckNumber: truckMetaDataForm.truckNumber,
+        //     name: truckMetaDataForm.name,
+        //     description: truckMetaDataForm.description,
+        //     tankCapacity: truckMetaDataForm.tankCapacity
+        //   })
+        // );
+
+        toast({
+          title: "Truck Meta Data Saved",
+          description: "Successful",
+          status: "success",
+          duration: 9000,
+          isClosable: true
+        });
+      }, Date.now().toString());
+    },
+    [awaitCallback]
+  );
 
   return (
     <Container maxW="xl" centerContent>
       <Box padding="4" bg={bg} maxW="6xl" maxH="4xl" resize="both" overflow="auto">
-        <AddTruckMetaData submitCallback={x => setTruckMetaDataForm(x)}></AddTruckMetaData>
+        <AddTruckMetaData submitCallback={x => saveForm(x)}></AddTruckMetaData>
       </Box>
     </Container>
   );
