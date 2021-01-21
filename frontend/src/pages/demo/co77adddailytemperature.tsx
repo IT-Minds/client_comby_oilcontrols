5078c6e71dfbcc8380f9c7fd230308fc33cf1add
Daniel Velando <dve@it-minds.dk>
2021-01-21 10:39:33 +0100

    Post form to backend

--- frontend/src/pages/demo/co77adddailytemperature.tsx
+++ frontend/src/pages/demo/co77adddailytemperature.tsx
@@ -1,35 +1,47 @@
 import { Box, Container, useColorModeValue, useToast } from "@chakra-ui/react";
 import AddDailyTemperatureComp from "components/DailyTemperature/AddDailyTemperature";
-import { AddDailyTemperatureForm } from "components/DailyTemperature/AddDailyTemperatureForm";
+import { useOffline } from "hooks/useOffline";
 import { Locale } from "i18n/Locale";
 import { GetStaticProps, NextPage } from "next";
 import { I18nProps } from "next-rosetta";
-import { useEffect, useState } from "react";
+import { useCallback } from "react";
+import { genDailyTemperatureClient } from "services/backend/apiClients";
+import { CreateDailyTemperatureCommand } from "services/backend/nswagts";
 
 const DemoPage: NextPage = () => {
-  const [addDailyTemperatureForm, setDailyTemperatureForm] = useState<AddDailyTemperatureForm>(
-    null
-  );
   const toast = useToast();
+  const { awaitCallback } = useOffline();
   const bg = useColorModeValue("gray.100", "gray.700");
 
-  useEffect(() => {
-    if (addDailyTemperatureForm) {
-      toast({
-        title: "Daily temperature added",
-        description: JSON.stringify(addDailyTemperatureForm),
-        status: "success",
-        duration: 9000,
-        isClosable: true
-      });
-    }
-  }, [addDailyTemperatureForm]);
+  const saveForm = useCallback(
+    async dailyTemperature => {
+      awaitCallback(async () => {
+        const client = await genDailyTemperatureClient();
+        await client.create(
+          new CreateDailyTemperatureCommand({
+            date: dailyTemperature.date,
+            regionId: dailyTemperature.regionId,
+            temperature: dailyTemperature.temperature
+          })
+        );
+
+        toast({
+          title: "Daily Temperature Created",
+          description: "Successful",
+          status: "success",
+          duration: 9000,
+          isClosable: true
+        });
+      }, Date.now().toString());
+    },
+    [awaitCallback]
+  );
 
   return (
     <Container maxW="xl" centerContent>
       <Box padding="4" bg={bg} maxW="6xl" maxH="4xl" resize="both" overflow="auto">
         <AddDailyTemperatureComp
-          submitCallback={x => setDailyTemperatureForm(x)}
+          submitCallback={x => saveForm(x)}
           regions={[
             { name: "Region 1", id: "1" },
             { name: "Region 2", id: "2" },
