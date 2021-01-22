dc6f1d409b0396bbc1abf786b8c07e60576de6cb
Jeppe Trip Kofoed <jtk@it-minds.dk>
2021-01-22 08:44:53 +0100

    added in the PredictDayReachingMinimumFuelLevel extension, which I removed by accident.

--- backend/Domain/EntityExtensions/LocationExtentions.cs
+++ backend/Domain/EntityExtensions/LocationExtentions.cs
@@ -80,5 +80,26 @@ namespace Domain.EntityExtensions
 
       return currentDate.AddDays(-1);
     }
+    public static DateTime PredictDayReachingMinimumFuelLevel(this Location location)
+    {
+      double limit = location.FuelTank.MinimumFuelAmount;
+      var sortedRefills = location.Refills.OrderBy(x => x.ActualDeliveryDate);
+      var newestRefill = sortedRefills.Last();
+      var refillDate = newestRefill.ActualDeliveryDate;
+      var fuelAmount = newestRefill.EndAmount;
+      var fuelConsumption = location.FuelConsumptionPerDegreeOfHeating();
+
+      var currentDate = refillDate;
+      do
+      {
+        currentDate = currentDate.AddDays(1);
+        var heatdegree = location.Region.DailyTemperatureEstimate(currentDate);
+        var fuelConsumed = heatdegree * fuelConsumption;
+        fuelAmount = fuelAmount - fuelConsumed;
+      }
+      while (fuelAmount > location.FuelTank.MinimumFuelAmount);
+
+      return currentDate.AddDays(-1);
+    }
   }
 }
