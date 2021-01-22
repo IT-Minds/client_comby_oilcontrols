560c16d9830aef006e17fa84abedbd023bfbf9ce
Jeppe Trip Kofoed <jtk@it-minds.dk>
2021-01-22 11:24:46 +0100

    added command to update a trucks metadata.

---
+++ backend/Application/Trucks/Commands/UpdateTruck/UpdateTruckCommand.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Trucks.Commands.UpdateTruck
+{
+  public class UpdateTruckCommand : IRequest<int>
+  {
+    public int Id { get; set; }
+    public string TruckIdentifier { get; set; }
+    public string Description { get; set; }
+    public double TankCapacity { get; set; }
+    public int StartRefillNumber { get; set; }
+    public class UpdateTruckCommandHandler : IRequestHandler<UpdateTruckCommand, int>
+    {
+      private readonly IApplicationDbContext _context;
+
+      public UpdateTruckCommandHandler(IApplicationDbContext context)
+      {
+        _context = context;
+      }
+
+      public async Task<int> Handle(UpdateTruckCommand request, CancellationToken cancellationToken)
+      {
+        var truck = await _context.Trucks
+          .Include(x => x.DailyStates)
+          .FirstOrDefaultAsync(x => x.Id == request.Id);
+        if (truck == null)
+        {
+          throw new ArgumentException("No truck with Id: " + request.Id + ".");
+        }
+
+        truck.TruckIdentifier = request.TruckIdentifier;
+        truck.Description = request.Description;
+        truck.TankCapacity = request.TankCapacity;
+        var dailyState = truck.DailyStates.OrderByDescending(x => x.Date).FirstOrDefault();
+        if (dailyState == null)
+        {
+          throw new ArgumentException("Currently no daily state registered for truck " + request.Id + ", and therefor not possible to update the Starting refill-number.");
+        }
+
+        await _context.SaveChangesAsync(cancellationToken);
+        return truck.Id;
+      }
+    }
+  }
+}
