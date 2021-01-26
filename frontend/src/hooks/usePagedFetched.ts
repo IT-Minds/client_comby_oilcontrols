560087811325e85150273ccbcae3f320a5237762
Daniel Velando <dve@it-minds.dk>
2021-01-26 17:04:52 +0100

    Change needle type

--- frontend/src/hooks/usePagedFetched.ts
+++ frontend/src/hooks/usePagedFetched.ts
@@ -2,12 +2,10 @@ import { Dispatch, useCallback, useEffect, useMemo, useState } from "react";
 import { AllListActions, ListReducerActionType } from "react-list-reducer";
 import { IPageResult } from "services/backend/ext/IPageResult";
 import { logger } from "utils/logger";
-
-type Callback<T> = (needle: string, size: number, sortBy: string, skip?: number) => Promise<T>;
-
-type Settings = {
+type Callback<T, U> = (needle: U, size: number, sortBy: string, skip?: number) => Promise<T>;
+type Settings<T = string> = {
   autoStart?: boolean;
-  initialNeedle?: string;
+  initialNeedle?: T;
   pageSize?: number;
 };
 const defaultSettings: Settings = {
@@ -15,28 +13,24 @@ const defaultSettings: Settings = {
   initialNeedle: "0",
   pageSize: 100
 };
-
-type Return = {
+type Return<T = unknown> = {
   done: boolean;
   error: boolean;
-  needle: string;
-  fetchData: (initialNeedle?: string, initialSkip?: number) => Promise<void>;
+  needle: T;
+  fetchData: (initialNeedle?: T, initialSkip?: number) => Promise<void>;
 };
-
-export const usePagedFetched = <T extends IPageResult<V>, V = unknown>(
+export const usePagedFetched = <T extends IPageResult<V, U>, U = string, V = unknown>(
   sortBy: string,
-  callback: Callback<T>,
+  callback: Callback<T, U>,
   dataDispatch: Dispatch<AllListActions<V>>,
-  initSettings: Settings = defaultSettings
-): Return => {
-  const settings = useMemo(() => Object.assign(defaultSettings, initSettings), []);
-
+  initSettings: Settings<U>
+): Return<U> => {
+  const settings = useMemo<Settings<U>>(() => Object.assign(defaultSettings, initSettings), []);
   const [done, setDone] = useState(!settings.autoStart);
   const [error, setError] = useState(false);
-  const [needle, setNeedle] = useState(settings.initialNeedle);
-
+  const [needle, setNeedle] = useState<U>(settings.initialNeedle);
   const fetchData = useCallback<Return["fetchData"]>(
-    async (initialNeedle: string = settings.initialNeedle, initialSkip?: number) => {
+    async (initialNeedle: U = settings.initialNeedle, initialSkip?: number) => {
       setDone(false);
       setError(false);
       try {
@@ -45,18 +39,15 @@ export const usePagedFetched = <T extends IPageResult<V>, V = unknown>(
         let needle = initialNeedle;
         let skip = initialSkip;
         let hasMore = needle !== null || skip !== null;
-
         while (hasMore) {
           if (pages > Number.parseInt(process.env.NEXT_PUBLIC_MAX_FETCH_PAGES))
             throw new Error("Pagination Max Reached!");
-
           logger.info("Fetching page", pages++);
           const pageResult = await callback(needle, settings.pageSize, sortBy, skip);
           resultSize = pageResult?.results ? pageResult.results.length : -1;
           needle = pageResult.newNeedle;
           hasMore = pageResult.hasMore;
           skip = 0;
-
           logger.debug("Page fetched", resultSize);
           if (needle) setNeedle(needle);
           if (resultSize > 0)
@@ -73,10 +64,8 @@ export const usePagedFetched = <T extends IPageResult<V>, V = unknown>(
     },
     [settings]
   );
-
   useEffect(() => {
     if (settings.autoStart) fetchData(settings.initialNeedle);
   }, [settings, fetchData]);
-
   return { done, error, fetchData, needle };
 };
