b43800b471bf73663703332b7afa0cef10a2bba6
Jeppe Trip Kofoed <jtk@it-minds.dk>
2021-01-27 08:54:25 +0100

    migration added to support debtors and locations.

--- backend/Infrastructure/Persistence/Migrations/20210127075315_M2MDebtorLocation.Designer.cs
+++ backend/Infrastructure/Persistence/Migrations/20210127075315_M2MDebtorLocation.Designer.cs
@@ -4,14 +4,16 @@ using Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210127075315_M2MDebtorLocation")]
+    partial class M2MDebtorLocation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -289,12 +291,6 @@ namespace Infrastructure.Persistence.Migrations
 
                     b.Property<string>("CreatedBy")
                         .HasColumnType("nvarchar(max)");
-
-                    b.Property<int>("DaysBetweenRefills")
-                        .HasColumnType("int");
-
-                    b.Property<double>("EstimateFuelConsumption")
-                        .HasColumnType("float");
 
                     b.Property<DateTimeOffset>("LastModified")
                         .HasColumnType("datetimeoffset");
