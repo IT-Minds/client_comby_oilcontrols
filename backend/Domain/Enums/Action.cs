02916a2f5805bc83b102eecd8d340019c5f2a224
Jeppe Trip Kofoed <jtk@it-minds.dk>
2021-01-28 14:06:22 +0100

    updated the action enum.

--- backend/Domain/Enums/Action.cs
+++ backend/Domain/Enums/Action.cs
@@ -2,6 +2,23 @@ namespace Domain.Enums
 {
   public enum Action
   {
-    READ = 0
+    ASSIGN_COUPON = 0,
+    SAVE_COUPON_IMAGE = 1,
+    UPDATE_COUPON_STATUS = 2,
+    GET_COUPONS = 3,
+    SET_TEMPERATURE = 4,
+    GET_DEBTOR = 5,
+    GET_LOCATION_HITORIES = 6,
+    CREATE_LOCATION = 7,
+    UPDATE_LOCATION = 8,
+    GET_LOCATION = 9,
+    CREATE_REFILL = 10,
+    ORDER_REFILL = 11,
+    GET_REFILLS = 12,
+    GET_STREETS = 13,
+    CREATE_TRUCK_REFILL = 14,
+    CREATE_TRUCK = 15,
+    UPDATE_TRUCK = 16,
+    GET_TRUCK = 17
   }
 }
