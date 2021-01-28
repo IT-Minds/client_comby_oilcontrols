adc518a71e887bc511a8853f7d75f75adc240b78
Jeppe Trip Kofoed <jtk@it-minds.dk>
2021-01-28 15:55:25 +0100

    added in command to remove debtor from location.

--- backend/Application/Locations/Commands/RemoveDebtorFromLocation/RemoveDebtorFromLocationCommand.cs
+++ backend/Application/Locations/Commands/RemoveDebtorFromLocation/RemoveDebtorFromLocationCommand.cs
@@ -1,7 +1,10 @@
+using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Common.Interfaces;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Locations.Commands.RemoveDebtorFromLocation
 {
@@ -18,9 +21,29 @@ namespace Application.Locations.Commands.RemoveDebtorFromLocation
       {
         _context = context;
       }
-      public Task<int> Handle(RemoveDebtorFromLocationCommand request, CancellationToken cancellationToken)
+      public async Task<int> Handle(RemoveDebtorFromLocationCommand request, CancellationToken cancellationToken)
       {
-        throw new System.NotImplementedException();
+        var location = await _context.Locations
+          .Include(e => e.Debtors)
+          .FirstOrDefaultAsync(x => x.Id == request.LocationId);
+        if (location == null)
+        {
+          throw new ArgumentException("No location with ID: " + request.LocationId);
+        }
+
+        var debtor = await _context.Debtors
+          .Include(e => e.Locations)
+          .FirstOrDefaultAsync(x => x.Id == request.DebtorId);
+        if (location == null)
+        {
+          throw new ArgumentException("No debtor with ID: " + request.DebtorId);
+        }
+
+        var locationDebtor = await _context.LocationDebtors.FirstOrDefaultAsync(x => x.LocationId == request.LocationId && x.DebtorId == request.DebtorId);
+        _context.LocationDebtors.Remove(locationDebtor);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return location.Id;
       }
     }
   }
