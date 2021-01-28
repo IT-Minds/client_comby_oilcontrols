ae34a00ed050022a4594d1a8bcaa4106b4a0c66b
Daniel Velando <dve@it-minds.dk>
2021-01-28 15:11:10 +0100

    Fetch trucks

--- frontend/src/pages/truck/index.tsx
+++ frontend/src/pages/truck/index.tsx
@@ -42,39 +42,14 @@ export const getStaticProps: GetStaticProps<I18nProps<Locale>> = async context =
   const locale = context.locale || context.defaultLocale;
 
   const { table = {} } = await import(`../../i18n/${locale}`);
-  const data = await genTruckClient().then(client =>
-    client.getTrucks(0).catch(() => {
-      return new PageResultOfTruckInfoIdDtoAndInteger({
-        hasMore: true,
-        newNeedle: 0,
-        pagesRemaining: 1,
-        results: [
-          new TruckInfoIdDto({
-            name: "Truck 1",
-            description: "A big truck",
-            refillNumber: 1,
-            tankCapacity: 123,
-            truckIdentifier: "1",
-            id: 0
-          }),
-          new TruckInfoIdDto({
-            name: "Truck 2",
-            description: "A small truck",
-            refillNumber: 2,
-            tankCapacity: 456,
-            truckIdentifier: "2",
-            id: 1
-          })
-        ]
-      });
-    })
-  );
+
+  const data = await genTruckClient().then(client => client.getTrucks(0));
 
   return {
     props: {
       table,
       // !This is a hack to get around undefined values in dataset
-      truckEntities: JSON.parse(JSON.stringify(data.results)),
+      trucksEntities: JSON.parse(JSON.stringify(data.results)),
       needle: data.newNeedle ?? "0",
       hasMore: data.hasMore,
       pageCount: data.pagesRemaining + 1
