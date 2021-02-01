34c44b4369c41e14af825b6ef75fb85002289c05
Jeppe Trip Kofoed <jtk@it-minds.dk>
2021-02-01 13:07:10 +0100

    figured out what to check in the authorization service, I think.

--- backend/Web/Services/AuthorizationService.cs
+++ backend/Web/Services/AuthorizationService.cs
@@ -22,30 +22,11 @@ namespace Web.Services
 
     public bool HasPolicy(Domain.Enums.Action policy)
     {
-      //TODO: Remove all the writelines when I have figures out how this is supposed to work.
-      var headers = _httpContextAccessor.HttpContext.Request.Headers.ToList();
-      Console.WriteLine("############## HEADERS");
-      foreach (var header in headers)
-      {
-        Console.WriteLine(header);
-      }
-      var items = _httpContextAccessor.HttpContext.Items.ToList();
-      Console.WriteLine("############## ITEMS");
-      foreach (var item in items)
-      {
-        Console.WriteLine("{0} : {1}", item.Key, item.Value);
-      }
-      Console.WriteLine("############# USER: {0}", _httpContextAccessor.HttpContext.User.ToString());
-      var allclaims = _httpContextAccessor.HttpContext?.User?.Claims.ToList();
-      Console.WriteLine("############## ALL CLAIMS {0}", allclaims.Count());
-      foreach (var claim in allclaims)
-      {
-        Console.WriteLine(claim);
-      }
-      var claims = _httpContextAccessor.HttpContext?.User?.Claims.Where(c => c.Type == "policies");
-      var policyMatch = claims.SelectMany(x => x.Value).ToList().Where(x => x.Equals(policy));
-      var result = policyMatch != null && policyMatch.Count() > 0;
-      return result;
+      var policies = _httpContextAccessor.HttpContext.Request.Headers
+        .Where(x => x.Key.Equals("Authorization"))
+        .Where(x => x.Value.Equals(policy.ToString()));
+
+      return policies != null && policies.Count() > 0;
     }
   }
 }
