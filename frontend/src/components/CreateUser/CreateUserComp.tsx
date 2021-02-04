473a1767ffb8553bbde0630bcca0a61f19d2b58d
Daniel Velando <dve@it-minds.dk>
2021-02-04 14:24:33 +0100

    Use create user interface

--- frontend/src/components/CreateUser/CreateUserComp.tsx
+++ frontend/src/components/CreateUser/CreateUserComp.tsx
@@ -1,109 +1,91 @@
-import {
-    Button,
-    Container,
-    FormControl,
-    FormErrorMessage,
-    FormLabel,
-    Input,
-    InputGroup,
-    InputRightAddon,
-    NumberInput,
-    NumberInputField,
-    Select,
-    VStack
-  } from "@chakra-ui/react";
-  import StreetSelector from "components/StreetSelector/StreetSelector";
-  import React, { FC, FormEvent, useCallback, useState } from "react";
-  import { MdCheck } from "react-icons/md";
-  import { RefillScheduleRecord } from "services/backend/ext/enumConvertor";
-  import { RefillSchedule, TankType } from "services/backend/nswagts";
-  import { capitalize } from "utils/capitalizeAnyString";
-  import { logger } from "utils/logger";
-import { CreateUserForm } from "./CreateUserForm";
-  
-  type Props = {
-    submitCallback: (createUserForm: CreateUserForm) => void;
-  };
-  
-  const CreateUserComp: FC<Props> = ({ submitCallback }) => {
-    const [localForm, setLocalForm] = useState<CreateUserForm>(
-      {
-          username: "",
-          password: ""
-      }
-    );
-  
-    const [formSubmitAttempts, setFormSubmitAttempts] = useState(0);
-  
-    const updateLocalForm = useCallback((value: unknown, key: keyof typeof localForm) => {
-      setLocalForm(form => {
-        (form[key] as unknown) = value;
-        return form;
-      });
-    }, []);
-  
-    const handleSubmit = useCallback(
-      (event: FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        logger.debug("Submitting form CreateUserComp");
-        submitCallback(localForm);
-        setFormSubmitAttempts(0);
-      },
-      [localForm]
-    );
-  
-    return (
-      <form onSubmit={handleSubmit}>
-        <VStack spacing={2}>
-           <FormControl isRequired isInvalid={formSubmitAttempts > 0 && !localForm.username}>
-            {
-              //TODO: translation
-            }
-            <FormLabel>Username</FormLabel>
-            <Input
-              placeholder="Username"
-              onChange={e => {
-                updateLocalForm(e.target.value, "username");
-              }}
-            />
-            {
-              //TODO: translation
-            }
-            <FormErrorMessage>Please enter a username</FormErrorMessage>
-          </FormControl>
+import { Button, FormControl, FormErrorMessage, FormLabel, Input, VStack } from "@chakra-ui/react";
+import React, { FC, FormEvent, useCallback, useState } from "react";
+import { MdCheck } from "react-icons/md";
+import { CreateUserCommand } from "services/backend/nswagts";
+import { logger } from "utils/logger";
 
-          <FormControl isRequired isInvalid={formSubmitAttempts > 0 && !localForm.password}>
-            {
-              //TODO: translation
-            }
-            <FormLabel>Password</FormLabel>
-            <Input
-            type="password"
-              placeholder="Password"
-              onChange={e => {
-                updateLocalForm(e.target.value, "password");
-              }}
-            />
-            {
-              //TODO: translation
-            }
-            <FormErrorMessage>Please enter a password</FormErrorMessage>
-          </FormControl>
-  
+type Props = {
+  submitCallback: (createUserForm: CreateUserCommand) => void;
+};
+
+const CreateUserComp: FC<Props> = ({ submitCallback }) => {
+  const [localForm, setLocalForm] = useState<CreateUserCommand>(
+    new CreateUserCommand({
+      userName: "",
+      password: ""
+    })
+  );
+
+  const [formSubmitAttempts, setFormSubmitAttempts] = useState(0);
+
+  const updateLocalForm = useCallback((value: unknown, key: keyof typeof localForm) => {
+    setLocalForm(form => {
+      (form[key] as unknown) = value;
+      return form;
+    });
+  }, []);
+
+  const handleSubmit = useCallback(
+    (event: FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      logger.debug("Submitting form CreateUserComp");
+      submitCallback(localForm);
+      setFormSubmitAttempts(0);
+    },
+    [localForm]
+  );
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <VStack spacing={2}>
+        <FormControl isRequired isInvalid={formSubmitAttempts > 0 && !localForm.userName}>
           {
             //TODO: translation
           }
-          <Button
-            colorScheme="green"
-            type="submit"
-            rightIcon={<MdCheck />}
-            onClick={() => setFormSubmitAttempts(x => x + 1)}>
-            Create user
-          </Button>
-        </VStack>
-      </form>
-    );
-  };
-  
-  export default CreateUserComp;
-  
+          <FormLabel>Username</FormLabel>
+          <Input
+            placeholder="Username"
+            onChange={e => {
+              updateLocalForm(e.target.value, "userName");
+            }}
+          />
+          {
+            //TODO: translation
+          }
+          <FormErrorMessage>Please enter a username</FormErrorMessage>
+        </FormControl>
+
+        <FormControl isRequired isInvalid={formSubmitAttempts > 0 && !localForm.password}>
+          {
+            //TODO: translation
+          }
+          <FormLabel>Password</FormLabel>
+          <Input
+            type="password"
+            placeholder="Password"
+            onChange={e => {
+              updateLocalForm(e.target.value, "password");
+            }}
+          />
+          {
+            //TODO: translation
+          }
+          <FormErrorMessage>Please enter a password</FormErrorMessage>
+        </FormControl>
+
+        {
+          //TODO: translation
+        }
+        <Button
+          colorScheme="green"
+          type="submit"
+          rightIcon={<MdCheck />}
+          onClick={() => setFormSubmitAttempts(x => x + 1)}>
+          Create user
+        </Button>
+      </VStack>
+    </form>
+  );
+};
+
+export default CreateUserComp;
