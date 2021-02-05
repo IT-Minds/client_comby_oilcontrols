b6abe59dcff0331976cf2a596b41669db3a71796
Jeppe Trip Kofoed <32454966+JeppeTrip@users.noreply.github.com>
2021-02-05 22:26:52 +0100

    CO-126 Update UserRoles (#125)
    Co-authored-by: Martin Johannesson <mjh@it-minds.dk>

---
+++ backend/Application/Users/Commands/UpdateUserRoles/UserRoleDto.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace Application.Users.Commands.UpdateUserRole
+{
+  public class UserRoleDto
+  {
+    public string Username { get; set; }
+    public string Role { get; set; }
+  }
+}
--- backend/Web/Controllers/UserController.cs
+++ backend/Web/Controllers/UserController.cs
@@ -1,6 +1,6 @@
-using System;
 using System.Threading.Tasks;
 using Application.Users.Commands.CreateUser;
+using Application.Users.Commands.UpdateUserRole;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Web.Controllers
@@ -12,5 +12,12 @@ namespace Web.Controllers
     {
       return await Mediator.Send(command);
     }
+
+    [HttpPut]
+    public async Task<ActionResult<UserRoleDto>> UpdateUserRoles(UpdateUserRolesCommand command)
+    {
+      return await Mediator.Send(command);
+    }
+
   }
 }
