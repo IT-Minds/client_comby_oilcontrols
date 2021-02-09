60f4e35facda28ddb1df8dda50bde178ce3aa60f
Daniel Velando <dve@it-minds.dk>
2021-02-09 15:11:58 +0100

    Show debtor info

---
+++ frontend/src/components/DebtorDetailsComp.tsx
@@ -0,0 +1,40 @@
+import {
+    Container,
+    HStack,
+    Switch,
+    Text,
+    VStack
+  } from "@chakra-ui/react";
+  import React, { FC, useState } from "react";
+  import { DebtorDto } from "services/backend/nswagts";
+  
+  type Props = {
+    debtorData: DebtorDto;
+  };
+  
+  const DebtorDetailsComp: FC<Props> = ({ debtorData }) => {
+    const [debtor, setDebtor] = useState<DebtorDto>(
+        debtorData ?? new DebtorDto({
+        })       
+    );
+  
+    return (
+      <Container w="100%" maxW="unset">
+          <VStack>
+                  <Text>Name: {debtor.name}</Text>
+                  <Text>Account number: {debtor.accountNumber}</Text>
+                  <Text>Blocked: {debtor.blocked}</Text>
+                  <HStack>
+                    <Text>Coupon is required: {debtor.couponRequired}</Text>
+                    <Switch
+                    id="couponRequired"
+                    //    onChange={e => updateLocalForm(e.target.checked, "isSpecialFill")}
+                    />
+                 </HStack>
+              </VStack>
+      </Container>
+    );
+  };
+  
+  export default DebtorDetailsComp;
+  
