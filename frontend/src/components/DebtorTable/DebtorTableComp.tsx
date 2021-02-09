ca0d4fbd99da00a0d487243cb0ed559b19ef5ca8
Daniel Velando <dve@it-minds.dk>
2021-02-09 13:51:21 +0100

    Remove pagination
    Remove pagination

--- frontend/src/components/DebtorTable/DebtorTableComp.tsx
+++ frontend/src/components/DebtorTable/DebtorTableComp.tsx
@@ -1,5 +1,4 @@
 import {
-    Button,
     Container,
     Table,
     TableCaption,
@@ -7,127 +6,30 @@ import {
     Td,
     Th,
     Thead,
-    Tr,
-    useToast
+    Tr
   } from "@chakra-ui/react";
-  import PageIndicator from "components/Demo/components/PageIndicator";
-  import { usePagedFetched } from "hooks/usePagedFetched";
-  import React, { FC, useEffect, useMemo, useReducer, useState } from "react";
-  import ListReducer from "react-list-reducer";
-import { genDebtorClient } from "services/backend/apiClients";
+  import React, { FC, useEffect, useReducer } from "react";
+  import ListReducer, { ListReducerActionType } from "react-list-reducer";
   import { DebtorDto } from "services/backend/nswagts";
   
   type Props = {
     preLoadedData?: DebtorDto[];
-    preloadDataNeedle?: string;
-    preloadLoadedAll?: boolean;
-    preLoadedPageCount?: number;
   };
-  
-  export const PAGE_SHOW_SIZE = 15;
-  
+   
   const DebtorTableComp: FC<Props> = ({
-    preLoadedData = [],
-    preloadDataNeedle = "",
-    preloadLoadedAll = false
+    preLoadedData = []
   }) => {
-    const toast = useToast();
-  
-    const [data, dataDispatch] = useReducer(ListReducer<DebtorDto>("dbId"), preLoadedData);
-    const [pageShowing, setPageShowing] = useState(0);
-  
-    {
-      //TODO: waiting for getUsers
-    }
-    // const done: any = null;
-    // const error: any = null;
-    const { done, error } = usePagedFetched(
-      "0",
-      (needle, size, _sortBy, skip) =>
-        genDebtorClient().then(client =>
-          client.get()
-        ),
-      dataDispatch,
-      {
-        autoStart: !preloadLoadedAll,
-        initialNeedle: preloadDataNeedle,
-        pageSize: PAGE_SHOW_SIZE
-      }
-    );
-  
-    const pages = useMemo(() => {
-      const maxDataLength = data.length;
-      const allPageMax = maxDataLength > 0 ? Math.ceil(maxDataLength / PAGE_SHOW_SIZE) : 0;
-  
-      return [...new Array(allPageMax)].map((x, i) => i);
-    }, [data, done]);
-  
-    /**
-     * Splits the data into table pages.
-     */
-    const dataSplitter = useMemo(() => {
-      const maxDataLength = data.length;
-      const realPageMax = data.length > 0 ? Math.ceil(data.length / PAGE_SHOW_SIZE) : 0;
-      const allPageMax = maxDataLength > 0 ? Math.ceil(maxDataLength / PAGE_SHOW_SIZE) : 0;
-  
-      if (allPageMax === 0) {
-        return {
-          data: []
-        };
-      }
-  
-      const realDataOnCurrentPage = data.slice(
-        pageShowing * PAGE_SHOW_SIZE,
-        (pageShowing + 1) * PAGE_SHOW_SIZE
-      );
-  
-      if (realDataOnCurrentPage.length >= PAGE_SHOW_SIZE) {
-        return {
-          data: realDataOnCurrentPage
-        };
-      }
-  
-      if (realDataOnCurrentPage.length > 0) {
-        const fillCount = realPageMax * PAGE_SHOW_SIZE - data.length;
-        console.assert(
-          fillCount === PAGE_SHOW_SIZE - realDataOnCurrentPage.length,
-          "Math is not right"
-        );
-  
-        return {
-          data: realDataOnCurrentPage
-        };
-      }
-  
-      return {
-        data: []
-      };
-    }, [data, pageShowing]);
-  
-    const elementsOnLastPage = useMemo(() => {
-      const elements = data.slice((pages.length - 1) * PAGE_SHOW_SIZE, pages.length * PAGE_SHOW_SIZE);
-      return elements.length;
-    }, [pages, data]);
-  
-    useEffect(() => {
-      if (done)
-        toast({
-          title: "All data loaded",
-          description: "All the pages for the table is now loaded",
-          status: "success",
-          duration: 3000,
-          isClosable: true
-        });
-      if (error)
-        toast({
-          title: "Error during data fetch!",
-          description: "An error occurred during fetch of all page data",
-          status: "error",
-          duration: 5000,
-          isClosable: true
-        });
-    }, [done, error]);
-  
+    const [data, dataDispatch] = useReducer(ListReducer<DebtorDto>("dbId"), preLoadedData ?? []);
+
+      useEffect(() => {
+        if (preLoadedData) {
+          dataDispatch({
+            type: ListReducerActionType.Reset,
+            data: preLoadedData
+          });
+        }
+      });
+    
     return (
       <Container w="100%" maxW="unset">
         <Table variant="striped" colorScheme="blue" size="sm">
@@ -138,20 +40,13 @@ import { genDebtorClient } from "services/backend/apiClients";
             </Tr>
           </Thead>
           <Tbody>
-            {dataSplitter.data.map(debtor => (
+            {data.map(debtor => (
               <Tr key={debtor.dbId}>
                 <Td>{debtor.name}</Td>
               </Tr>
             ))}
           </Tbody>
         </Table>
-        {pages.length > 1 && (
-          <PageIndicator activePage={pageShowing} onClickPage={setPageShowing} pages={pages}>
-            {!done && elementsOnLastPage >= PAGE_SHOW_SIZE && (
-              <Button colorScheme="blue" size="sm" variant="outline"></Button>
-            )}
-          </PageIndicator>
-        )}
       </Container>
     );
   };
--- frontend/src/pages/demo/co155debtortable.tsx
+++ frontend/src/pages/demo/co155debtortable.tsx
@@ -5,26 +5,20 @@ import { GetStaticProps, NextPage } from "next";
 import { I18nProps } from "next-rosetta";
 import React from "react";
 import { genDebtorClient } from "services/backend/apiClients";
-import { PageResultOfLocationDebtorHistoryDtoAndDateTime, DebtorDto } from "services/backend/nswagts";
+import { DebtorDto } from "services/backend/nswagts";
 
 type Props = {
   debtorEntities: DebtorDto[];
-  needle: string;
-  hasMore: boolean;
-  pageCount: number;
 };
 
-const DemoPage: NextPage<Props> = ({ debtorEntities, needle, hasMore, pageCount }) => {
+const DemoPage: NextPage<Props> = ({ debtorEntities }) => {
   const bg = useColorModeValue("gray.100", "gray.700");
 
   return (
     <Container maxW="xl" centerContent>
       <Box padding="4" bg={bg} maxW="6xl" maxH="4xl" resize="both" overflow="auto">
         <DebtorTableComp
-          preLoadedData={debtorEntities}
-          preloadDataNeedle={needle}
-          preloadLoadedAll={!hasMore}
-          preLoadedPageCount={pageCount}></DebtorTableComp>
+          preLoadedData={debtorEntities}></DebtorTableComp>
       </Box>
     </Container>
   );
@@ -35,28 +29,12 @@ export const getStaticProps: GetStaticProps<I18nProps<Locale>> = async context =
 
   const { table = {} } = await import(`../../i18n/${locale}`);
 
-    const data = await genDebtorClient().then(client =>
-      client.get().catch(() => {
-        return new PageResultOfLocationDebtorHistoryDtoAndDateTime({
-          hasMore: true,
-          newNeedle: new Date(),
-          pagesRemaining: 1,
-          results: []
-        });
-      })
-    );
+    const data = await genDebtorClient().then(client => client.get());
 
-  {
-    //TODO: waiting 'data'
-  }
   return {
     props: {
       table,
-      // !This is a hack to get around undefined values in dataset
-       debtorEntities: JSON.parse(JSON.stringify(data)),
-       needle: data.newNeedle ?? "0",
-       hasMore: data.hasMore,
-       pageCount: data.pagesRemaining + 1
+       debtorEntities: data
     },
     revalidate: 60
   };
