b1dfba519f928bfab10fdf32c84b5d1d3354b2bd
Daniel Velando <dve@it-minds.dk>
2021-02-09 15:40:53 +0100

    Update coupon required

--- frontend/src/components/DebtorDetailsComp.tsx
+++ frontend/src/components/DebtorDetailsComp.tsx
@@ -1,25 +1,56 @@
-import { Container, HStack, Switch, Text, VStack } from "@chakra-ui/react";
-import React, { FC, useState } from "react";
-import { DebtorDto } from "services/backend/nswagts";
+import { Container, HStack, Switch, Text, useToast, VStack } from "@chakra-ui/react";
+import React, { FC, useCallback, useState } from "react";
+import { genDebtorClient } from "services/backend/apiClients";
+import { DebtorDto, IDebtorDto, PrintCouponRequiredCommand } from "services/backend/nswagts";
 
 type Props = {
-  debtorData: DebtorDto;
+  debtorData: IDebtorDto;
 };
 
 const DebtorDetailsComp: FC<Props> = ({ debtorData }) => {
-  const [debtor, setDebtor] = useState<DebtorDto>(debtorData ?? new DebtorDto({}));
+  const [debtor, setDebtor] = useState<IDebtorDto>(debtorData ?? {});
+  const toast = useToast();
+  
+  const saveCouponRequired = useCallback(
+    async (isRequired: boolean) => {
+      const client = await genDebtorClient();
+      await client.printCouponRequired(
+            debtor.dbId,
+            new PrintCouponRequiredCommand({
+            debtorId: debtor.dbId,
+            printCouponRequired: isRequired
+            })
+      ).then(() => {
+          setDebtor(
+            {...debtor,
+            couponRequired: isRequired
+            });
+      });
+      toast({
+        title: "Debtor Updated",
+        description: "Successful",
+        status: "success",
+        duration: 9000,
+        isClosable: true
+      });
+    },
+    [debtor]
+  );
+
+  
 
   return (
     <Container w="100%" maxW="unset">
       <VStack>
         <Text>Name: {debtor.name}</Text>
         <Text>Account number: {debtor.accountNumber}</Text>
-        <Text>Blocked: {debtor.blocked}</Text>
+        <Text>Blocked: {debtor.blocked ? "true" : "false"}</Text>
         <HStack>
           <Text>Coupon is required: {debtor.couponRequired}</Text>
           <Switch
+            isChecked={debtor.couponRequired}
             id="couponRequired"
-            //    onChange={e => updateLocalForm(e.target.checked, "isSpecialFill")}
+                onChange={e => saveCouponRequired(e.target.checked)}
           />
         </HStack>
       </VStack>
