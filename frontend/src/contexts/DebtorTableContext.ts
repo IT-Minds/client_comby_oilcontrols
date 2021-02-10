f40c77620913b6557382f6386aec1ca908bf3435
Daniel Velando <dve@it-minds.dk>
2021-02-10 08:32:11 +0100

    Added debtor context

---
+++ frontend/src/contexts/DebtorTableContext.ts
@@ -0,0 +1,9 @@
+import { useAuth } from "hooks/useAuth";
+import { createContext } from "react";
+import { IDebtorDto } from "services/backend/nswagts";
+
+type ContextType = {
+  debtorUpdated: (debtor: IDebtorDto) => void;
+};
+
+export const DebtorTableContext = createContext<ContextType>(null);
