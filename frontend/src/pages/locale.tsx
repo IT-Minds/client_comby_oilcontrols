dfce4b064106d442da80005efb38342b7f35ea09
Daniel Velando <dve@it-minds.dk>
2021-02-15 08:43:21 +0100

    Added open/close arrow

--- frontend/src/pages/locale.tsx
+++ frontend/src/pages/locale.tsx
@@ -13,7 +13,8 @@ import ConsumptionTableComp from "components/Consumption/ConsumptionTableComp";
 import { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 import { I18nProps, useI18n } from "next-rosetta";
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
+import { MdArrowDropDown, MdArrowDropUp } from "react-icons/md";
 import { genStatsClient } from "services/backend/apiClients";
 import { downloadFile } from "utils/downloadFile";
 
@@ -25,6 +26,8 @@ type Props = {
 
 const LocalePage: NextPage<Props> = () => {
   const { t } = useI18n<Locale>();
+
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
   const download = useCallback(async () => {
     const client = await genStatsClient();
@@ -50,8 +53,16 @@ const LocalePage: NextPage<Props> = () => {
           <Button onClick={download}>Download Refill</Button>
           <ConsumptionTableComp></ConsumptionTableComp>
 
-          <Menu>
-            <MenuButton as={Button}>Download History</MenuButton>
+          <Menu
+            onOpen={() => {
+              setIsOpen(true);
+            }}
+            onClose={() => {
+              setIsOpen(false);
+            }}>
+            <MenuButton as={Button} rightIcon={isOpen ? <MdArrowDropUp /> : <MdArrowDropDown />}>
+              Download History
+            </MenuButton>
             <MenuList>
               <MenuItem onClick={() => downloadUsageHistory(0)}>Måned</MenuItem>
               <MenuItem onClick={() => downloadUsageHistory(1)}>Kvartal</MenuItem>
