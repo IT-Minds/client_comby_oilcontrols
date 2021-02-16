49074c1c531e3e503fcb4de4299c3c091fff5fec
Daniel Velando <dve@it-minds.dk>
2021-02-16 13:09:32 +0100

    Move dropdown into table component

--- frontend/src/components/Consumption/ConsumptionTableComp.tsx
+++ frontend/src/components/Consumption/ConsumptionTableComp.tsx
@@ -1,34 +1,83 @@
-import { Container, Table, TableCaption, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
+import {
+  Button,
+  Container,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Table,
+  TableCaption,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr
+} from "@chakra-ui/react";
 import { useI18n } from "next-rosetta";
-import React, { FC, useEffect, useReducer } from "react";
+import React, { FC, useCallback, useEffect, useReducer, useState } from "react";
+import { MdArrowDropDown, MdArrowDropUp } from "react-icons/md";
 import ListReducer, { ListReducerActionType } from "react-list-reducer";
+import { genStatsClient } from "services/backend/apiClients";
+import { IntervalRecord } from "services/backend/ext/enumConvertor";
 import { FuelConsumptionDto } from "services/backend/nswagts";
 
 type Props = {
-  preLoadedData?: FuelConsumptionDto[];
+  locationId: number;
 };
 
-const ConsumptionTableComp: FC<Props> = ({ preLoadedData = [] }) => {
+const ConsumptionTableComp: FC<Props> = ({ locationId }) => {
   const { t } = useI18n<Locale>();
+  const [interval, setInterval] = useState(null);
+  const [fuelConsumptionEntities, setFuelConsumptionEntities] = useState<FuelConsumptionDto[]>(
+    null
+  );
+
+  const previousYear = new Date();
+  previousYear.setFullYear(previousYear.getFullYear() - 1);
 
   const [data, dataDispatch] = useReducer(
     ListReducer<FuelConsumptionDto>("locationId"),
-    preLoadedData ?? []
+    fuelConsumptionEntities ?? []
   );
 
   useEffect(() => {
-    if (preLoadedData) {
+    if (fuelConsumptionEntities) {
       dataDispatch({
         type: ListReducerActionType.Reset,
-        data: preLoadedData
+        data: fuelConsumptionEntities
       });
     }
-  }, [preLoadedData]);
+  }, [fuelConsumptionEntities]);
+
+  const getTableData = useCallback(async (interval: number) => {
+    setInterval(interval);
+    const data = await genStatsClient().then(client =>
+      client.getUsageHistory(locationId, interval, previousYear, new Date())
+    );
+    setFuelConsumptionEntities(data);
+  }, []);
 
   return (
     <Container w="100%" maxW="unset">
+      <Menu>
+        {({ isOpen }) => (
+          <>
+            <MenuButton as={Button} rightIcon={isOpen ? <MdArrowDropUp /> : <MdArrowDropDown />}>
+              {interval != null ? t("enums.interval." + interval) : t("localePage.tableInterval")}
+            </MenuButton>
+            <MenuList>
+              {Object.entries(IntervalRecord).map(([a, b]) => (
+                <MenuItem key={b} onClick={() => getTableData(b)}>
+                  {t("enums.interval." + b)}
+                </MenuItem>
+              ))}
+            </MenuList>
+          </>
+        )}
+      </Menu>
       <Table variant="striped" colorScheme="blue" size="sm">
         <TableCaption placement="top">{t("consumptionTable.consumptionHistory")}</TableCaption>
+
         <Thead>
           <Tr>
             <Th>{t("consumptionTable.address")}</Th>
