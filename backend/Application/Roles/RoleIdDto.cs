a96c3607e824be0077291157991582a9d911f4e5
Martin Johannesson <mjh@it-minds.dk>
2021-02-16 18:56:53 +0100

    Role mapping

--- backend/Application/Roles/RoleIdDto.cs
+++ backend/Application/Roles/RoleIdDto.cs
@@ -1,10 +1,18 @@
+using Application.Common.Mappings;
 using AutoMapper;
 using Domain.Entities;
 
 namespace Application.Roles
 {
-  public class RoleIdDto : RoleDto
+  public class RoleIdDto : RoleDto, IAutoMap<Role>
   {
     public int Id { get; set; }
+
+    public void Mapping(Profile profile)
+    {
+      profile.CreateMap<Role, RoleIdDto>()
+        .IncludeBase<Role, RoleDto>();
+      ;
+    }
   }
 }
