83a6a9dcd25b06fbc433211d6bce94d89318defc
Martin Johannesson <mjh@it-minds.dk>
2021-02-22 10:44:12 +0100

    Reverted lint update

--- frontend/src/hoc/authGuardHOC.ts
+++ frontend/src/hoc/authGuardHOC.ts
@@ -2,10 +2,10 @@ import { UserTypeContext } from "contexts/UserTypeContext";
 import { ComponentType, useContext } from "react";
 import { Action } from "services/backend/nswagts";
 
-export const authGuardHOC = <P extends object>(
+export const authGuardHOC = <P extends Record<string, unknown>>(
   WrappedComponent: ComponentType<P>,
   ...actions: Action[]
-) => {
+): ComponentType<P> => {
   const { activeUser } = useContext(UserTypeContext);
 
   if (actions.every(a => activeUser.currentRole.actions.includes(a))) {
