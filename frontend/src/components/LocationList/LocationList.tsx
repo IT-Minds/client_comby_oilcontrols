be9a11693a87024c31750015b2bb89f216b4deb0
Daniel Velando <dve@it-minds.dk>
2021-02-23 08:17:17 +0100

    Implement sorting

--- frontend/src/components/LocationList/LocationList.tsx
+++ frontend/src/components/LocationList/LocationList.tsx
@@ -1,11 +1,12 @@
-import { HStack, Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
+import { HStack, Spacer, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
 import RefillModalBtn from "components/FillOutRefillForm/RefillModalBtn";
 import EditLocationTriggerBtn from "components/LocaleMetaDataForm/EditLocationTriggerBtn";
 import ViewLocationHistoryModalBtn from "components/LocationHistory/ViewLocationHistoryModalBtn";
 import OrderRefillComp from "components/OrderRefill/OrderRefillComp";
+import QuerySortBtn, { Direction } from "components/SortFilter/QuerySortBtn";
 import { useColors } from "hooks/useColors";
 import { useI18n } from "next-rosetta";
-import { FC, useCallback } from "react";
+import React, { FC, useCallback, useState } from "react";
 import { genRefillClient } from "services/backend/apiClients";
 import {
   IOrderRefillCommand,
@@ -28,18 +29,54 @@ const LocationList: FC<Props> = ({ data }) => {
     await client.orderRefill(new OrderRefillCommand(orderRefillForm));
   }, []);
 
+  const defaultSort = (a: LocationDetailsIdDto, b: LocationDetailsIdDto) => (a.id > b.id ? 1 : -1);
+
+  const [sort, setSort] = useState<(a: LocationDetailsIdDto, b: LocationDetailsIdDto) => number>(
+    () => defaultSort
+  );
+
+  const sortCb = useCallback((key: keyof LocationDetailsIdDto, direction: Direction) => {
+    if (direction === null) {
+      setSort(() => defaultSort);
+      return;
+    }
+
+    const sortVal = direction === "ASC" ? 1 : -1;
+    setSort(() => (a: LocationDetailsIdDto, b: LocationDetailsIdDto) =>
+      a[key] > b[key] ? sortVal : -sortVal
+    );
+  }, []);
+
   return (
     <Table size="sm">
       <Thead>
         <Tr>
-          <Th>{t("locationList.address")}</Th>
-          <Th>{t("locationList.regionId")}</Th>
-          <Th>{t("locationList.scheduleType")}</Th>
+          <Th>
+            <HStack spacing={1}>
+              <Text>{t("locationList.address")}</Text>
+              <Spacer />
+              <QuerySortBtn queryKey="address" sortCb={sortCb} />
+            </HStack>
+          </Th>
+          <Th>
+            <HStack spacing={1}>
+              <Text>{t("locationList.regionId")}</Text>
+              <Spacer />
+              <QuerySortBtn queryKey="regionId" sortCb={sortCb} />
+            </HStack>
+          </Th>
+          <Th>
+            <HStack spacing={1}>
+              <Text>{t("locationList.scheduleType")}</Text>
+              <Spacer />
+              <QuerySortBtn queryKey="scheduleType" sortCb={sortCb} />
+            </HStack>
+          </Th>
           <Th></Th>
         </Tr>
       </Thead>
       <Tbody>
-        {data.map(dat => (
+        {data.sort(sort).map(dat => (
           <Tr
             key={dat.id}
             _hover={{
