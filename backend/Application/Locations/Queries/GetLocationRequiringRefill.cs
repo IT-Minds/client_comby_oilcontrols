32b65ba6cd0a5adbd57d97c3c8e501d96940b288
Jeppe Trip Kofoed <jtk@it-minds.dk>
2021-03-01 13:26:28 +0100

    updated the query such that it actually sets the DebtorBlocked attribute

--- backend/Application/Locations/Queries/GetLocationRequiringRefill.cs
+++ backend/Application/Locations/Queries/GetLocationRequiringRefill.cs
@@ -7,6 +7,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Application.Common.Interfaces;
 using Application.Common.Security;
+using Application.Common.Services;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using Domain.Entities.Refills;
@@ -25,22 +26,41 @@ namespace Application.Locations.Queries
     {
       private readonly IApplicationDbContext _context;
       private readonly IMapper _mapper;
+      private readonly SynchronizeDebtorService _synchronizeDebtorService;
 
-      public GetLocationRequiringRefillHandler(IApplicationDbContext context, IMapper mapper)
+      public GetLocationRequiringRefillHandler(IApplicationDbContext context, IMapper mapper, IUniContaService uniContaService, SynchronizeDebtorService synchroniceDebtorService)
       {
         _context = context;
         _mapper = mapper;
+        _synchronizeDebtorService = synchroniceDebtorService;
       }
 
       public async Task<List<LocationRefillDto>> Handle(GetLocationRequiringRefill request, CancellationToken cancellationToken)
       {
-        var refillDtos = await _context.AssignedRefills
+        var refillEntities = _context.AssignedRefills
           .Include(r => r.Truck)
           .Include(r => r.Location)
             .ThenInclude(l => l.FuelTank)
-          .Where(r => r.TruckId == request.TruckId)
+          .Where(r => r.TruckId == request.TruckId);
+        var refillDtos = await refillEntities
           .ProjectTo<LocationRefillDto>(_mapper.ConfigurationProvider)
           .ToListAsync();
+
+        var result = await _synchronizeDebtorService.SyncroniceDebtor();
+
+        foreach (var (a, b) in result)
+        {
+          var debtorsRefills = refillEntities.Where(x => x.Location.Debtors.Where(x => x.DebtorId == b.Id).Count() != 0);
+
+          foreach (var entity in debtorsRefills)
+          {
+            var dtos = refillDtos.Where(x => x.LocationId == entity.Location.Id);
+            foreach (var dto in dtos)
+            {
+              dto.DebtorBlocked = a.Blocked;
+            }
+          }
+        }
 
         return refillDtos;
       }
